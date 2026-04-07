fae18caaf635cdaa3a0c5ba0cb31ea3a1bc0d173
Yuusupport <ycalfred19@gmail.com>
2026-04-07 10:24:13 -0400

    added stuff to the test file

--- test._main.py
+++ test._main.py
@@ -1,7 +1,14 @@
 import main
 import pytest
 
-def test_foo():
-    assert main_foo(0, 0) == pytest.approx(1)
-    assert main_foo(1,0) == 1
-    assert main_foo(0,1) == 1
+@pytest.mark.parametrize(
+        ('input_x', 'input_y', 'expected'),
+        (
+            (0, 0, 1),
+            (1, 0, 1),
+            (0, 1, 1),
+            (1, 1, 2)
+        )
+)
+def test_foo(input_x, input_y, expected):
+    assert main.foo(input_x, input_y) == expected
